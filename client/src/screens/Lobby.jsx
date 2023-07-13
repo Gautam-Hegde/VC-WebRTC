a2d143571fbc6e0490c27b1ce5a423fdcb5a6afa
Gautam-Hegde <gautamatjob@protonmail.com>
2023-07-13 18:53:38 +0530

    r fs

--- client/src/screens/Lobby.jsx
+++ client/src/screens/Lobby.jsx
@@ -1,4 +1,5 @@
-import React,{useState,useCallback,useEffect} from 'react'
+import React,{useState,useCallback,useEffect} from 'react';
+import { useNavigate } from 'react-router-dom';
 import {useSocket} from "../context/SocketProvider";
 
 const LobbyScreen = () => {
@@ -7,6 +8,8 @@ const LobbyScreen = () => {
   const[room,setRoom]=useState("");
 
   const socket=useSocket();
+const navigate=useNavigate();
+
   console.log(socket);
   const handleSubmitForm=useCallback((e)=>{
     e.preventDefault();
@@ -15,13 +18,21 @@ const LobbyScreen = () => {
   },[email,room,socket]);
 
 
-  
+  const handleJoinRoom=useCallback((data)=>{
+      const {email,room}=data;
+      // console.log(email,room);
+      navigate(`/room/${room}`);
+  },[navigate])
 
   useEffect(()=>{
-socket.on("room:join",(data)=>{
-  console.log(`data from backend ${data}`);
-})
-  },[socket]);
+// socket.on("room:join",(data)=>{
+//   console.log(`data from backend ${data}`);
+// })
+socket.on("room:join",handleJoinRoom);
+return()=>{
+  socket.off('room:join',handleJoinRoom);
+}
+  },[socket,handleJoinRoom]);
 
   return (
     <div>
