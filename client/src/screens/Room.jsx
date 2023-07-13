9f36ee64f4148e91e306c5cd01d6516411f866dd
Gautam-Hegde <gautamatjob@protonmail.com>
2023-07-13 19:17:07 +0530

    emit user join

--- client/src/screens/Room.jsx
+++ client/src/screens/Room.jsx
@@ -1,9 +1,20 @@
-import React,{useEffect} from 'react'
+import React,{useCallback, useEffect,useState} from 'react'
 import { useSocket } from '../context/SocketProvider';
 
 const RoomPage = () => {
 
 const socket=useSocket();
+
+const handleUserJoined=useCallback(({email,id})=>{
+console.log(`Email ${email} joined the room`);
+},[])
+
+useEffect(()=>{
+socket.on('user:joined',handleUserJoined);
+return()=>{
+    socket.off('user:joined',handleUserJoined);
+}
+},[socket,handleUserJoined]);
 
   return (
     <div>
